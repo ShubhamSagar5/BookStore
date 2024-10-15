dcac6312f205557b15262fee0d985bcf6f3af678
shubham <shubhamsagar628@gmail.com>
2024-10-15 14:02:49 +0530

    deleteBook,updateBookDetails,getSingleBook,getAllBookList api implemented

--- Backend/src/controller/StoreController.js
+++ Backend/src/controller/StoreController.js
@@ -34,6 +34,102 @@ const addNewBook = async(req,res) => {
     }
 }
 
+const getAllBookList = async(req,res) => {
+    try {
+        
+        const books = await Book.find() 
+
+        return res.status(200).json({
+            success:true,
+            message:"List of Book",
+            data:books
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+} 
+
+const getSingleBook  = async(req,res)=>{
+    try {
+        
+        const {bookId} = req.params
+
+        const book = await Book.findById(bookId) 
+
+        return res.status(200).json({
+            success:true,
+            message:"Deatils of book fetch successfully",
+            data:book
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+const updateBookDetails = async(req,res) => {
+    try {
+        
+        const {bookId} = req.params 
+
+        const {bookName,description,price,author,image} = req.body
+
+        const book = await Book.findByIdAndUpdate(bookId,{
+            bookName,
+            description,
+            price,
+            image,
+            author
+        },{new:true})
+
+        return res.status(200).json({
+            success:true,
+            message:"Book Details Update Successfully",
+            data:book
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+const deleteBook = async(req,res) => {
+    try {
+        
+        const {bookId} = req.params 
+
+        const book = await Book.findByIdAndDelete(bookId) 
+
+        return res.status(200).json({
+            success:true,
+            message:"Book Delete Successfully",
+            data:book
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
 module.exports ={
-    addNewBook
+    addNewBook,
+    getAllBookList,
+    getSingleBook,
+    updateBookDetails,
+    deleteBook
+
 }
--- Backend/src/router/StoreRouter.js
+++ Backend/src/router/StoreRouter.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const { addNewBook } = require('../controller/StoreController')
+const { addNewBook, getAllBookList, getSingleBook,updateBookDetails, deleteBook } = require('../controller/StoreController')
 
 const bookRouter = express.Router() 
 
 bookRouter.post("/addBook",addNewBook)
+bookRouter.get("/getBooks",getAllBookList)
+bookRouter.get("/getBook/:bookId",getSingleBook)
+bookRouter.post("/updateBook/:bookId",updateBookDetails)
+bookRouter.delete("/deleteBook/:bookId",deleteBook)
 
 module.exports = bookRouter
